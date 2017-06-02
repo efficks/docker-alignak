c451fc0bcd72b7aceb0396f3732a41bb0255c736
François-Xavier Choinière <fx@efficks.com>
2017-06-02 15:28:48 -0400

    webui and utilisation de yaml

--- build.sh
+++ build.sh
@@ -2,33 +2,29 @@
 set -e
 set -x
 
-rm -rf build
+declare -a MODULES=("arbiter" "broker" "poller" "reactionner" "receiver" "scheduler")
 
-declare -A MODULES
-MODULES["arbiter"]="7770"
-MODULES["broker"]="7772"
-MODULES["poller"]="7771"
-MODULES["reactionner"]="7769"
-MODULES["receiver"]="7773"
-MODULES["scheduler"]="7768"
-
-for MODULE in "${!MODULES[@]}"
+for MODULE in "${MODULES[@]}"
 do
 	mkdir -p build/$MODULE
-if [ "$MODULE" == "arbiter" ]
-then
-	m4 -D MODULENAME=$MODULE -D MODULEPORT=${MODULES[$MODULE]} Dockerfile.$MODULE > build/$MODULE/Dockerfile
-else
-	m4 -D MODULENAME=$MODULE -D MODULEPORT=${MODULES[$MODULE]} Dockerfile.other > build/$MODULE/Dockerfile
-fi
-	cd build/$MODULE;
+	jinja2 -D MODULENAME=$MODULE Dockerfile.common config.yaml > build/$MODULE/Dockerfile
+        cd build/$MODULE;
 	docker build --network=host -t alignak-$MODULE .
 	cd -
 done
 
+# Backend
 mkdir -p build/backend
 cp Dockerfile.backend build/backend/Dockerfile
 cp alignakbackend.py build/backend
 cd build/backend
 docker build --network=host -t alignak-backend .
+cd -
+
+# Webui
+mkdir -p build/webui
+cp Dockerfile.webui build/webui/Dockerfile
+#cp alignakbackend.py build/backend
+cd build/webui
+docker build --network=host -t alignak-webui .
 cd -
